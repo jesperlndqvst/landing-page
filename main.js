889cdb359145ff39f29b0e3fae453a2699dc677f
emeliepetersson <emepet1204@skola.goteborg.se>
2019-11-07 11:43:41 +0100

    add javascript to mobile nav items

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // Function toggles between class names to show/hide menu items.
 function toggleMenu(event) {
-	const menuItems = event.currentTarget.parentElement.querySelector('.mobile-nav-items');
-	menuItems.classList.toggle('show-menu');
+    const menuItems = event.currentTarget.parentElement.querySelector('.menu-items');
+    menuItems.classList.toggle('show-menu');
 }
 
 // Add onclick event to element with class name "hamburger-icon".
